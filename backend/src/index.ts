4475ec762a587892121fd493995bfb55388bbb02
shishir <shishirtech506@gmail.com>
2025-07-27 15:44:06 +0530

    added ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -15,7 +16,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8800;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
